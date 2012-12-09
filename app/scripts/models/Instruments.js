08353411c6067b8753a133efc3a7cf56d74a2f72
David Choi <daaave@gmail.com>
2012-12-09 06:42:16 -0500

    Add Instruments.js, refactor FretboardView
    Mostly committing as a branch off point.  tQuery is causing problems
    because its source isn't easy to read, and its documentation may as
    well not exist.  More and more, defaulting to vanilla three.js.

---
+++ app/scripts/models/Instruments.js
@@ -0,0 +1,90 @@
+// Requires DMusic.js
+
+GuitarTrainer.String = Ember.Object.extend({
+	root: DMusic.Note.create(),
+	numFrets: 22,
+	notes: null,
+
+	init: function(){
+		this._super();
+
+		var root = this.get("root");
+		var notes = [root];
+		var i, len = this.get("numFrets");
+		var lastNote = root;
+		for(i=0; i<len; i++){
+			var newNote = lastNote.sharpenedNote(1);
+			notes.push(newNote);
+			lastNote = newNote;
+		}
+		this.set("notes", notes);
+	},
+
+	print: function(){
+		var notes = this.get("notes");
+		var len = notes.length;
+
+		var buff = "";
+		for(var i=0; i<len; i++){
+			var note = notes[i];
+			buff += note.get("name") + note.get("octave") + " (" + note.get("frequency") + ")";
+		}
+		console.log(buff);
+	}
+});
+
+/*
+	Instrument is a generalized fretted instrument class that can be subclassed
+	with different defaults for different instruments or tunings.
+*/
+
+GuitarTrainer.Instrument = Ember.Object.extend({
+	strings: [],	/*
+						Array of GuitarTrainer.String objects.
+						Any disparity between string fret counts imply they are shifted down toward the bridge
+						e.g. The banjo's 5th string is 5 frets shorter than the rest.
+					*/
+
+	tuning: [],	//	Array of root notes for the strings
+	stringLengths: [],
+	totalLength: function(){
+		return Math.max.apply(null, this.get("stringLengths"));
+	}.property("stringLengths"),
+
+	init: function(){
+		this._super();
+		var strings = [];
+		var tuning = this.get("tuning");
+		var stringLengths = this.get("stringLengths");
+		var i, len = tuning.length;
+		for(i=0; i<len; i++){
+			var stringLen = stringLengths[i];
+			var string = GuitarTrainer.String.create({root: tuning[i], numFrets: stringLen});
+			strings.push(string);
+		}
+		this.set("strings", strings);
+	},
+
+	print: function(){
+		var strings = this.get("strings");
+		var len = strings.length;
+		for(var i=0; i<len; i++){
+			var string = strings[i];
+			string.print();
+		}
+	}
+});
+
+GuitarTrainer.Guitar = GuitarTrainer.Instrument.create({
+	tuning: [
+		DMusic.Note.create({name: "E", octave: 2}),
+		DMusic.Note.create({name: "A", octave: 2}),
+		DMusic.Note.create({name: "D", octave: 3}),
+		DMusic.Note.create({name: "G", octave: 3}),
+		DMusic.Note.create({name: "B", octave: 3}),
+		DMusic.Note.create({name: "E", octave: 4})
+	],
+	stringLengths: [24, 24, 24, 24, 24, 24]
+});
+
+var guitar = GuitarTrainer.Guitar;
--- app/scripts/views/FretboardView.js
+++ app/scripts/views/FretboardView.js
@@ -1,26 +1,196 @@
 /*
 	To Do:
-		Add go to fret method
+		Divide strings into segments for heatmapping
+
+		Setup note track with automatic scrolling.
 */
+
+function randomColor(){
+	return Math.floor(Math.random() * 0xffffff);
+}
+
+
+/*
+	World class handles scene creation, and basic camera interface.
+*/
+GuitarTrainer.World = Ember.Object.extend({
+	world: null,
+	camera: null,
+	camLight: null,
+
+	init: function(){
+		this._super();
+		var world = tQuery.createWorld().boilerplate();
+		world.tRenderer().setClearColorHex( 0x000000, world.tRenderer().getClearAlpha());
+		//tQuery.createAmbientLight().color(0xffffff).addTo(world);
+		var camera = world._camera;
+		var light = tQuery.createPointLight().position(0, 0, 0).color(0xffffff);
+		light.addTo(world);
+
+		this.set("world", world);
+
+		this.set("camera", camera);
+		this.set("tQCamera", tQuery(camera));
+		this.set("camLight", light);
+	},
+
+	tweenPosBy: function(obj, vector, time){
+		var currPos = obj.position;
+		var target = {
+			x: currPos.x + vector.x,
+			y: currPos.y + vector.y,
+			z: currPos.z + vector.z
+		};
+		new TWEEN.Tween(obj.position).to(target, time).easing(TWEEN.Easing.Quadratic.InOut).start();
+	},
+
+	add: function(tq){
+		tq.addTo(this.get("world"));
+	},
+
+	tweenPosTo: function(obj, target, time){
+		new TWEEN.Tween(obj.position).to(target, time).easing(TWEEN.Easing.Quadratic.InOut).start();
+	},
+
+	panTo: function(pos, time){
+		var camera = this.get("camera");
+		var camLight = this.get("camLight");
+		var target = {
+			x: pos.x || camera.position.x,
+			y: pos.y || camera.position.y,
+			z: pos.z || camera.position.z
+		};
+		this.tweenPosTo(camera, target, time);
+		this.tweenPosTo(camLight, target, time);
+	},
+
+	panBy: function(vector, time){
+		var camera = this.get("camera");
+		var camLight = this.get("camLight");
+		this.tweenPosBy(camera, vector, time);
+		this.tweenPosBy(camLight, vector, time);
+	},
+
+	panRight: function(){
+		this.panBy({x: 5, y: 0, z: 0}, 200);
+	},
+
+	panLeft: function(){
+		this.panBy({x: -5, y: 0, z: 0}, 200);
+	},
+
+	zoomIn: function(){
+		this.panBy({x: 0, y: 0, z: -5}, 200);
+	},
+
+	zoomOut: function(){
+		this.panBy({x: 0, y: 0, z: 5}, 200);
+	},
+
+	turnLeft: function(){
+		this.get("camera").rotate(0, 0.05, 0);
+	},
+
+	turnRight: function(){
+		this.get("camera").rotate(0, -0.05, 0);
+	},
+
+	start: function(){
+		var world = this.get("world");
+		world.start();
+		world.removeCameraControls();
+		this.get("tQCamera").rotate(-0.2, 0.25, 0);
+		world.loop().hook(function(){
+			TWEEN.update();
+		});
+
+		this.panTo({x: 12, y: 7, Z: 1}, 200);
+	},
+
+	stop: function(){
+		this.get("world").stop();
+	}
+});
+
+GuitarTrainer.StringView = Ember.Object.extend({
+	world: null,
+	string: null,
+	color: 0xff0000,
+	opacity: 1,
+	phongMaterial: function(){
+		return new THREE.MeshPhongMaterial({
+			color: this.get("color"),
+			opacity: this.get("opacity")
+		});
+	}.property("color"),
+	lambertMaterial: function(){
+		return new THREE.MeshLambertMaterial({
+			color: this.get("color"),
+			opacity: this.get("opacity")
+		});
+	}.property("color"),
+	diameter: 0.1,
+	segments: null,
+	fretPositions: null,
+	yPos: 0,
+	zPos: 0,
+
+	init: function(){
+		this._super();
+		this.segments = [];
+		this.makeSegments();
+	},
+
+	makeSegments: function(){
+		var world = this.get("world");
+		var string = this.get("string");
+		var segments = this.get("segments");
+		var numSegments = string.get("numFrets");
+		var fretPositions = this.get("fretPositions");
+		var phongMaterial = this.get("phongMaterial");
+		var lambertMaterial = this.get("lambertMaterial");
+		var diameter = this.get("diameter");
+		var halfPi = Math.PI/2;
+		// fretOffset is the number of skipped frets for any irregular instruments (such as a banjo)
+		var fretOffset = fretPositions.length - numSegments - 1; // Subtract one, because there is a 0 fret at the nut.
+		var yPos = this.get("yPos"), zPos = this.get("zPos");
+		for(var i=0; i<numSegments; i++){
+			var fretIndex = fretOffset + i;
+			var fretPos1 = fretPositions[fretIndex], fretPos2 = fretPositions[fretIndex + 1];
+			var fretLength = fretPos2 - fretPos1;
+			var fretCenter = (fretPos1 + fretPos2) / 2;
+			var segment = tQuery.createCylinder((i%2)?phongMaterial:lambertMaterial).addClass("string").rotate(0, 0, halfPi).scale(diameter, fretLength, diameter).translate(fretCenter, yPos, zPos);
+			//segment.get(0).material.color.setRGB(1, 1, 1);
+			segments.push(segment);
+			world.add(segment);
+		}
+	}
+});
 
 GuitarTrainer.FretboardView = Ember.Object.extend({
 	world: null,
-	fretboardLength: 50,
-	numFrets: 22,
+	instrument: null,
+	/*
+		stringLength is the distance between the bridge and the nut for the longest string,
+		which extends beyond the fretboard.
+		Frets are assumed to be aligned for irregular strings (such as on a banjo)
+	*/
+	stringLength: 50,
+
 	fretPositions: function(){
 		var k = Math.pow(2, 1/12);
 		var divisor = 1;
-		var boardLength = this.fretboardLength;
-		var numFrets = this.numFrets;
+		var stringLength = this.get("stringLength");
+		var numFrets = this.get("instrument").get("totalLength");
 		var positions = [];
 
 		for(var i=0; i<=numFrets; i++){
-			var distFromBridge = boardLength/divisor;
-			positions.push(boardLength - distFromBridge);
+			var distFromBridge = stringLength/divisor;
+			positions.push(stringLength - distFromBridge);
 			divisor *= k;
 		}
 		return positions;
-	}.property("fretboardLength", "numFrets"),
+	}.property("stringLength", "numFrets"),
 
 	dotPositions: function(){
 		var fretPositions = this.get("fretPositions");
@@ -38,24 +208,24 @@ GuitarTrainer.FretboardView = Ember.Object.extend({
 		return positions;
 	}.property("fretPositions"),
 
-	camera: function(){
-		return tQuery(this.world._camera);
-	}.property("world"),
-
-	setupWorld: function(){
-		this.set("world", tQuery.createWorld().boilerplate());
-		this.makeStrings();
-		this.makeFrets();
-		this.makeDots();
-	},
-
 	makeStrings: function(){
 		var world = this.get("world");
 		var halfPi = Math.PI/2;
 		var colors = [0xff00ff, 0x00ff00, 0xff8800, 0x0000ff, 0xffff00, 0xff0000];
+		var fretPositions = this.get("fretPositions");
 
 		for(var i=0; i<6; i++){
-			var newString = tQuery.createCylinder(new THREE.MeshBasicMaterial({color: colors[i], opacity: 1})).addClass("string").addTo(world).rotate(0, 0, halfPi).scale(0.1, 50, 0.1).translate(25, i*0.55, -18);
+			//var newString = tQuery.createCylinder(new THREE.MeshBasicMaterial({color: colors[i], opacity: 1})).addClass("string").rotate(0, 0, halfPi).scale(0.1, 50, 0.1).translate(25, i*0.55, -18);
+			//world.add(newString);
+			var string = this.get("instrument").get("strings")[i];
+			var newString = GuitarTrainer.StringView.create({
+				world: world,
+				string: string,
+				fretPositions: fretPositions,
+				color: colors[i],
+				yPos: i * 0.55,
+				zPos: -18
+			});
 		}
 	},
 
@@ -67,8 +237,12 @@ GuitarTrainer.FretboardView = Ember.Object.extend({
 
 		for(var i=0; i<len; i++){
 			var x = fretPositions[i];
-			var fret = tQuery.createCube(new THREE.MeshBasicMaterial({color: 0x888888, opacity: 1})).addClass("fret").addTo(world).scale(0.1, 3.5, 0.1).translate(x, 1.4, -17.8);
-			var track = tQuery.createCube(new THREE.MeshBasicMaterial({color: 0xaaaaff})).addClass("track").addTo(world).scale(0.05, 0.05, 100).translate(x, 0, -67.8);
+
+			var fret = tQuery.createCube(new THREE.MeshLambertMaterial({color: 0x888888, opacity: 1})).addClass("fret").scale(0.1, 3.5, 0.1).translate(x, 1.4, -17.8);
+			world.add(fret);
+
+			var track = tQuery.createCube(new THREE.MeshPhongMaterial({color: 0xaaaaff})).addClass("track").scale(0.05, 0.05, 100).translate(x, 0, -67.8);
+			world.add(track);
 		}
 	},
 
@@ -79,27 +253,19 @@ GuitarTrainer.FretboardView = Ember.Object.extend({
 
 		for(var i=0; i<len; i++){
 			var x = dotPositions[i];
-			console.log(x);
-			var dot = tQuery.createSphere(new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 1})).addClass("dot").addTo(world).scale(0.2, 0.2, 0.2).translate(x, 1.4, -17.8);
+			var dot = tQuery.createSphere(new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 1})).addClass("dot").scale(0.2, 0.2, 0.2).translate(x, 1.4, -17.8);
+			world.add(dot);
 		}
 	},
 
-	tweenPosBy: function(obj, vector, time){
-		var currPos = obj.position;
-		var target = {
-			x: currPos.x + vector.x,
-			y: currPos.y + vector.y,
-			z: currPos.z + vector.z
-		};
-		new TWEEN.Tween(obj.position).to(target, time).easing(TWEEN.Easing.Quadratic.InOut).start();
-	},
-
-	tweenPosTo: function(obj, target, time){
-		new TWEEN.Tween(obj.position).to(target, time).easing(TWEEN.Easing.Quadratic.InOut).start();
+	drawInstrument: function(){
+		this.makeStrings();
+		this.makeFrets();
+		this.makeDots();
 	},
 
 	panToFret: function(fret){
-		console.log("target: " + fret);
+		var world = this.get("world");
 		if(fret > this.numFrets){
 			fret = this.numFrets;
 		}
@@ -107,73 +273,35 @@ GuitarTrainer.FretboardView = Ember.Object.extend({
 			fret = 0;
 		}
 		var targetX = this.get("fretPositions")[fret];
-		var camera = this.world._camera;
-		var target = {x: targetX, y: camera.position.y, z: camera.position.z};
-		//target.x = targetX;
-		this.tweenPosTo(this.world._camera, target, 200);
-	},
-
-	panRight: function(){
-		this.tweenPosBy(this.world._camera, {x: 5, y: 0, z: 0}, 200);
-	},
-
-	panLeft: function(){
-		this.tweenPosBy(this.world._camera, {x: -5, y: 0, z: 0}, 200);
-	},
-
-	zoomIn: function(){
-		this.tweenPosBy(this.world._camera, {x: 0, y: 0, z: -5}, 200);
-	},
-
-	zoomOut: function(){
-		this.tweenPosBy(this.world._camera, {x: 0, y: 0, z: 5}, 200);
-	},
-
-	turnLeft: function(){
-		this.get("camera").rotate(0, 0.05, 0);
-	},
-
-	turnRight: function(){
-		this.get("camera").rotate(0, -0.05, 0);
-	},
-
-	start: function(){
-		var world = this.get("world");
-		world.start();
-		world.removeCameraControls();
-		this.get("camera").rotate(-0.2, 0.25, 0).translate(12, 7, 1);
-		world.loop().hook(function(){
-			TWEEN.update();
-		});
-	},
-
-	stop: function(){
-		this.get("world").stop();
+		world.panTo({x: targetX}, 200);
 	}
-
 });
 
-var fretboard = GuitarTrainer.FretboardView.create();
-fretboard.setupWorld();
-fretboard.start();
+
+var world = GuitarTrainer.World.create();
+world.start();
+var fretboard = GuitarTrainer.FretboardView.create({world: world, instrument: GuitarTrainer.Guitar});
+
+fretboard.drawInstrument();
+
 $(document).keydown(function(e){
 	if(e.keyCode == 65){ // A
-		fretboard.panLeft();
+		world.panLeft();
 	}
 	if(e.keyCode == 68){ // D
-		fretboard.panRight();
+		world.panRight();
 	}
 	if(e.keyCode == 83){ // S
-		fretboard.zoomOut();
+		world.zoomOut();
 	}
 	if(e.keyCode == 87){ // W
-		fretboard.zoomIn();
+		world.zoomIn();
 	}
 	if(e.keyCode == 81){ // Q
-		fretboard.turnLeft();
+		world.turnLeft();
 	}
 	if(e.keyCode == 69){ // E
-		fretboard.turnRight();
+		world.turnRight();
 	}
 	if(e.keyCode >= 48 && e.keyCode <= 57){ // Num keys
 		var keyVal = e.keyCode - 48;
