e44f25d1812cc31647503d6ec775d387ccd19d96
David Choi <daaave@gmail.com>
2012-12-14 12:32:44 -0500

    Add bloom shader and anti-aliasing effects
    Also, destroyed performance.

--- app/scripts/views/FretboardView.js
+++ app/scripts/views/FretboardView.js
@@ -97,6 +97,7 @@ GuitarTrainer.ShapeFactory = Ember.Object.create({
 GuitarTrainer.World = Ember.Object.extend({
 	scene: null,
 	renderer: null,
+	composer: null,
 	camera: null,
 	camLight: null,
 
@@ -106,12 +107,15 @@ GuitarTrainer.World = Ember.Object.extend({
 		//world.tRenderer().setClearColorHex( 0x000000, world.tRenderer().getClearAlpha());
 		var $container = $("#exercise");
 		var height = $container.height(), width = $container.width();
-		console.log(width + " " + height);
-
-		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize(width, height);
-
 		var scene = new THREE.Scene();
+
+		var renderer = new THREE.WebGLRenderer({
+			clearColor: 0x000000,
+			clearAlpha: 1,
+			antialias: false
+		});
+		renderer.setSize(width, height);
+		renderer.autoClear = false;
 
 		var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
 		camera.position = {x: 12, y: 7, z: 0};
@@ -124,7 +128,21 @@ GuitarTrainer.World = Ember.Object.extend({
 		scene.add(camera);
 		scene.add(light);
 
+		var renderPass = new THREE.RenderPass(scene, camera);
+		var effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+		effectFXAA.uniforms[ 'resolution' ].value.set( 1 / window.innerWidth, 1 / window.innerHeight );
+		var effectBloom = new THREE.BloomPass(1.3);
+		var effectCopy = new THREE.ShaderPass(THREE.CopyShader);
+		effectCopy.renderToScreen = true;
+
+		var composer = new THREE.EffectComposer(renderer);
+		composer.addPass(renderPass);
+		composer.addPass(effectFXAA);
+		composer.addPass(effectBloom);
+		composer.addPass(effectCopy);
+
 		this.set("renderer", renderer);
+		this.set("composer", composer);
 		this.set("scene", scene);
 		this.set("camera", camera);
 		this.set("camLight", light);
@@ -146,8 +164,14 @@ GuitarTrainer.World = Ember.Object.extend({
 	},
 
 	render: function(){
+		var renderer = this.get("renderer");
+		var composer = this.get("composer");
+		var scene = this.get("scene");
+		var camera = this.get("camera");
 		TWEEN.update();
-		this.get("renderer").render(this.scene, this.camera);
+		//renderer.render(scene, camera);
+		renderer.clear();
+		composer.render();
 	},
 
 	add: function(obj){
