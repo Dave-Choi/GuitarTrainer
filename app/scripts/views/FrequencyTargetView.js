bffe83b4a8ee43a8d77ad378ff909fd686ad2208
David Choi <daaave@gmail.com>
2013-01-03 11:51:35 -0500

    Add a visual stem to frequency targets, and visual indication of targets having been hit
    The stem improves on the ability to read what frets targets are on, and
    help with reading their timing.

--- app/scripts/views/FrequencyTargetView.js
+++ app/scripts/views/FrequencyTargetView.js
@@ -11,8 +11,36 @@ GuitarTrainer.FrequencyTargetView = Ember.Object.extend({
 		var position = this.get("position");
 		var dimensions = this.get("dimensions");
 		var color = this.get("color");
-		var node = GuitarTrainer.ShapeFactory.cube({width: dimensions.x * 0.8, height: dimensions.y * 0.8, depth: dimensions.z * 0.8, color: color});
+		var node = new THREE.Object3D();
 		node.position = position;
+		var box = GuitarTrainer.ShapeFactory.cube({
+			width: dimensions.x * 0.8,
+			height: dimensions.y * 0.8,
+			depth: dimensions.z * 0.8,
+			color: color
+		});
+		var stem = GuitarTrainer.ShapeFactory.cylinder({
+			radiusTop: 0.1,
+			radiusBottom: 0.1,
+			height: position.y,
+			color: color
+		});
+		stem.position.y = -position.y / 2;
+		node.add(box);
+		node.add(stem);
 		this.set("threeNode", node);
-	}
+	},
+
+	targetHit: function(){
+		var target = this.get("target");
+		if(target.get("hasBeenHit")){
+			var children = this.get("threeNode").children;
+			var i, len = children.length;
+			for(i=0; i<len; i++){
+				var child = children[i];
+				child.material.transparent = true;
+				child.material.opacity = 0.1;
+			}
+		}
+	}.observes("target.hasBeenHit")
 });
