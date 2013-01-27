366b0900a8d26cd8f42b899c03b97a86c375e3be
David Choi <daaave@gmail.com>
2013-01-27 06:34:24 -0500

    Add generic 3D view base class.

---
+++ app/scripts/views/ThreeView.js
@@ -0,0 +1,12 @@
+GuitarTrainer.ThreeView = Ember.Object.extend({
+	threeNode: null,
+
+	init: function(){
+		var threeNode = new THREE.Object3D();
+		this.set("threeNode", threeNode);
+	},
+
+	add: function(node){
+		this.threeNode.add(node);
+	}
+});
