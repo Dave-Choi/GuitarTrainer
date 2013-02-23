2cd7d1fa5e8c4c07d02ef5d32807f90b4311df7e
David Choi <daaave@gmail.com>
2013-02-23 00:27:13 -0500

    Add ConditionalGraph class for dealing with exercise states and transitions.

---
+++ app/scripts/models/ExerciseGraph.js
@@ -0,0 +1,14 @@
+/*
+	Conditional graph that basically works as a state machine
+	for an exercise.
+
+	Opted not to use Ember.StateManager because it doesn't have
+	enough control over what type of transition causes what behavior.
+
+	In this case, transitions need to know what the source and destination states are
+	to evaluate whether they should transition at all, and what actions need to be taken.
+*/
+
+GuitarTrainer.ExerciseGraph = ConditionalGraph.extend({
+
+});
