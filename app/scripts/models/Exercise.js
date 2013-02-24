3acdf026c834a01935f31867040e76e29aea0f86
David Choi <daaave@gmail.com>
2013-02-24 15:38:06 -0500

    Add description property to Exercise model and template

--- app/scripts/models/Exercise.js
+++ app/scripts/models/Exercise.js
@@ -4,9 +4,14 @@
 */
 
 GuitarTrainer.Exercise = DS.Model.extend({
-	title: DS.attr("string")
+	title: DS.attr("string"),
+	description: DS.attr("string")
 });
 
 GuitarTrainer.Exercise.FIXTURES = [
-	{ id: 1, title: "A major scale" }
+	{
+		id: 1,
+		title: "A major scale",
+		description: "15 note major scale, played up and down 3 times."
+	}
 ];
