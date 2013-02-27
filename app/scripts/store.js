f93a9baddbfc530901f68795f6885c737dcdccb9
David Choi <daaave@gmail.com>
2013-02-27 00:43:56 -0500

    Add comment about DS.FixtureAdapter.
    It's not a standard adapter.  It was packed in with someone's packaged
    Ember Data, and is handy for delaying having to make a backend.

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,5 +1,5 @@
 // Requires Ember-Data
 GuitarTrainer.Store = DS.Store.extend({
   revision: 11,
-  adapter: "DS.FixtureAdapter"
+  adapter: "DS.FixtureAdapter" // Simulates remote calls.  Looks for a FIXTURES property on the model and loads data from there.
 });
