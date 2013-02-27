0d0f41834f09e1ff7188a8300931d6236eb89c26
David Choi <daaave@gmail.com>
2013-02-27 00:41:26 -0500

    Remove ConditionalGraph class

--- app/scripts/models/ExerciseGraph.js
+++ app/scripts/models/ExerciseGraph.js
@@ -9,15 +9,82 @@
 	to evaluate whether they should transition at all, and what actions need to be taken.
 */
 
-GuitarTrainer.ExerciseGraph = ConditionalGraph.extend({
+GuitarTrainer.ExerciseGraph = DS.Model.extend({
+	nodes: DS.hasMany("GuitarTrainer.ExerciseNode"),
 
+	init: function(){
+		this._super();
+		this.set("nodes", this.get("nodes") || []);
+	},
+
+	addNode: function(node){
+		var nodes = this.get("nodes");
+		nodes.push(node);
+		if(nodes.length == 1){
+			this.set("currentNode", node);
+		}
+	},
+
+	nextNode: function(){
+		var currentNode = this.get("currentNode");
+		return currentNode.nextNode();
+	},
+
+	advance: function(){
+		// Go to the next node
+		var next = this.nextNode();
+		this.set("currentNode", next);
+
+		// Return the new position.
+		return next;
+	}
 });
 
 
-GuitarTrainer.ExerciseNode = ConditionalGraph.Node.extend({
-	section: null
+GuitarTrainer.ExerciseNode = DS.Model.extend({
+	section: DS.hasMany("GuitarTrainer.ExerciseTransition"),
+	transitions: null,
+
+	init: function(){
+		this.set("transitions", this.get("transitions") || []);
+	},
+
+	addNewTransition: function(node, condition){
+		if(!condition){
+			condition = function(){
+				return true;
+			};
+		}
+		var transition = ConditionalGraph.Transition.create({
+			sourceNode: this,
+			targetNode: node,
+			shouldTransition: condition
+		});
+		this.get("transitions").push(transition);
+	},
+
+	addTransition: function(transition){
+		thsi.get("transitions").push(transition);
+	},
+
+	nextNode: function(){
+		var transitions = this.get("transitions");
+		var i, len = transitions.length;
+		for(i=0; i<len; i++){
+			var transition = transitions[i];
+			if(transition.shouldTransition()){
+				return transition.get("targetNode");
+			}
+		}
+		return null;
+	}
 });
 
-GuitarTrainer.ExerciseTransition = ConditionalGraph.Transition.extend({
+GuitarTrainer.ExerciseTransition = DS.Model.extend({
+	sourceNode: null,
+	targetNode: null,
 
+	shouldTransition: function(){
+		return true;
+	}
 });
