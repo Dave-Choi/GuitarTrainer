5983b823ba6fa3976a3783c3acac2058c62f1103
Dave Choi <daaave@gmail.com>
2013-03-01 06:20:07 -0500

    Update store to handle simple objects

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,5 +1,17 @@
 // Requires Ember-Data
+
+GuitarTrainer.StoreAdapter = DS.FixtureAdapter;
+
+GuitarTrainer.StoreAdapter.registerTransform("object", {
+	deserialize: function(serialized){
+		return Ember.none(serialized) ? {} : serialized;
+	},
+	serialize: function(deserialized){
+		return Ember.none(deserialized) ? {} : deserialized;
+	}
+});
+
 GuitarTrainer.Store = DS.Store.extend({
   revision: 11,
-  adapter: "DS.FixtureAdapter" // Simulates remote calls.  Looks for a FIXTURES property on the model and loads data from there.
+  adapter: "GuitarTrainer.StoreAdapter" // Simulates remote calls.  Looks for a FIXTURES property on the model and loads data from there.
 });
