8fa85894ed045018263d931daa73fba6df280031
Dave Choi <daaave@gmail.com>
2013-03-05 05:01:52 -0500

    Add method to ExercisePlayController to check models are actually loaded.

--- app/scripts/controllers/ExercisePlayController.js
+++ app/scripts/controllers/ExercisePlayController.js
@@ -3,5 +3,9 @@ GuitarTrainer.ExercisePlayController = Ember.ObjectController.extend({
 	content: function(){
 		// This buys me access to the model's properties via just their name in getters.
 		return this.get("controllers.exercise");
-	}.property("controllers.exercise")
+	}.property("controllers.exercise"),
+
+	modelsLoaded: function(){
+		return (this.get("content.isLoaded") && this.get("content.instrument.isLoaded"));
+	}.property("content.isLoaded", "content.instrument.isLoaded")
 });
