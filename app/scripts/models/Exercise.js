cb307ce6d2230c8774eccea11b5f39a6b4f062ff
Dave Choi <daaave@gmail.com>
2013-03-15 09:37:16 -0400

    Add Graph reference to Exercise model

--- app/scripts/models/Exercise.js
+++ app/scripts/models/Exercise.js
@@ -1,12 +1,13 @@
 /*
-	Exercise maintains a graph of ExerciseNodes, and provides functionality
-	to traverse it.
+	Exercise maintains a graph of ExerciseNodes, and meta information about the
+	exercise.
 */
 
 GuitarTrainer.Exercise = DS.Model.extend({
 	title: DS.attr("string"),
 	description: DS.attr("string"),
-	instrument: DS.belongsTo("GuitarTrainer.Instrument")
+	instrument: DS.belongsTo("GuitarTrainer.Instrument"),
+	graph: DS.belongsTo("GuitarTrainer.ExerciseGraph")
 });
 
 GuitarTrainer.Exercise.FIXTURES = [
@@ -14,6 +15,7 @@ GuitarTrainer.Exercise.FIXTURES = [
 		id: 1,
 		title: "A major scale",
 		description: "15 note major scale, played up and down 3 times.",
-		instrument: 1
+		instrument: 1,
+		graph: 1
 	}
 ];
